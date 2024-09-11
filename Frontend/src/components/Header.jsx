888aafac94f0a94eb653b12f046e4809c8ad73e1
eliseo hinojosa <eliseohinojosa@hotmail.es>
2024-09-11 13:06:29 -0300

    3cer_intento_1.2

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,25 +1,42 @@
-import {Link} from "react-router-dom";
-import logo from "../assets/logo.svg"
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 import Navbar from "./Navbar";
 import { useState } from "react";
+import { MdClose, MdMenu } from "react-icons/md";
+import { FaOpencart } from "react-icons/fa";
 const Header = () => {
-
-    const [menuOpened, setMenuOpened] = useState(true);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const toggleMenu = () => setMenuOpened(!menuOpened);
   return (
-   <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10">
-
-    <div className="px-4 flexBetween py-3 max-xs:px-2">
-        
+    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10">
+      <div className="px-4 flexBetween py-3 max-xs:px-2">
         <div>
-           <Link><img src={logo} alt="" height={66} width={88}/></Link> 
+          <Link>
+            <img src={logo} alt="" height={66} width={88} />
+          </Link>
         </div>
-        <Navbar containerStyles={"hiden md:flex gap-x-5 xl:gap-x-10 medium-15 "} />
+        <Navbar
+          containerStyles={"hidden md:flex gap-x-5 xl:gap-x-10 medium-15 "}
+        />
         /*Navbar mobile*/
-        <Navbar containerStyles={`${menuOpened ? "flex item-start flex-col gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64medium-16 right-1 " : ""}`} />
-        <div>buttons</div>
-    </div>
-   </header>
-  )
+        <Navbar
+          containerStyles={`${
+            menuOpened
+              ? "flex item-start flex-col gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300"
+              : "flex item-start flex-col gap-y-12 fixed top-20 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 -right-[100%]"
+          }`}
+        />
+        <div className="flexBetween sm:gap-x-6 bold-16">
+          {!menuOpened? (
+            <MdMenu className="md:hidden cursor-pointer hover:text-secondary mr-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full" onClick={toggleMenu}/>
+          ) : (
+          <MdClose className="md:hidden cursor-pointer hover:text-secondary mr-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full" onClick={toggleMenu}/>)}
+          <div className="flexBetween sm:gap-x-6"></div>
+          <Navbar to={"cart-page"} className={"flex"}><FaOpencart /></Navbar>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
