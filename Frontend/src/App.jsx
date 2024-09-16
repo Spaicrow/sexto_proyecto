ae9b0f10a556720ba1efe0a3b5d7b60bf6618e54
eliseo hinojosa <eliseohinojosa@hotmail.es>
2024-09-16 19:47:51 -0300

    3cer_intento_1.6

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,10 @@ import Cart from "./pages/Cart";
 import Product from "./pages/Product"
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
-
+//import imagenes
+import bannermens from "./assets/bannermens.png"
+import bannerwomen from "./assets/bannerwomens.png"
+import bannerkids from "./assets/bannerkids.png"
 
 export default function App() {
   return (
@@ -15,9 +18,9 @@ export default function App() {
         <Header />
         <Routes>
           <Route  path="/" element={<Home />}/>
-          <Route  path="/mens" element={<Category />}/>
-          <Route  path="/womens" element={<Category />}/>
-          <Route  path="/kids" element={<Category />}/>
+          <Route  path="/mens" element={<Category category="men" banner={bannermens}/>}/>
+          <Route  path="/womens" element={<Category category="women" banner={bannerwomen}/>}/>
+          <Route  path="/kids" element={<Category category="kid" banner={bannerkids}/>}/>
           <Route  path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />}/>
           </Route>
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,22 @@
-
+import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+        <h3>Sign Up</h3>
+        <div>
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+          
+        </div>
+        
+        <button>Continue</button>
+        <p>Already have an acount? <span>Login</span></p>
+      </div>
+
+    </section>
   )
 }
 
